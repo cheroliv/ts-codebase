00cbd59aac2fd4027e1ec851aa30fdbffccda84a
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-23 20:41:30 +0200

    add sttic methode to memo ts

--- 01_intro_ts/05_classes_interfaces.ts
+++ 01_intro_ts/05_classes_interfaces.ts
@@ -1,3 +1,12 @@
+/**
+ * Les classes:
+ * members,
+ * visibility,
+ * nommage,
+ * accesseurs
+ * scope identifier: static,private,protected
+ */
+
 class Member {
     username: String = "";
     email: String = "";
@@ -19,4 +28,28 @@ class Member {
     set password(new_password: String) {
         this._password = new_password;
     }
+
+    /**
+     * Une fonction static qui initilise un member,
+     * à partir d'un author
+     */
+    static fromAuthor(author: {
+        first_name: String,
+        last_name: String,
+        lang: String,
+        century: Number
+    }): Member {
+        return new Member(
+            `${author.first_name}.${author.last_name}`,
+            `${author.first_name}.${author.last_name}@acme.com`,
+        );
+    }
 }
+
+
+let domenico = Member.fromAuthor(author);
+
+console.assert(domenico.email === "Domenico.Losurdo@acme.com");
+domenico.password = "test";
+
+console.assert(domenico.password === "test");
