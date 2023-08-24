69ac8cc3b2db0fc9f34e8bab40dc4971a6f0ddf9
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-25 00:58:54 +0200

    add pokemon folder

--- 01_intro_ts/05_classes_interfaces.ts
+++ 01_intro_ts/05_classes_interfaces.ts
@@ -5,16 +5,23 @@
  * nommage,
  * accesseurs
  * scope identifier: static,private,protected
+ * 
+ * convention de nommage :
+ * Les membres private sont préfixés avec un _ * 
  */
 
-class Member {
+// Interface donne un contrat à un type
+interface IPerson {
+    username: String;
+    email: String;
+    readonly signup_date: Date;
+}
+
+class Member implements IPerson {
     username: String = "";
     email: String = "";
-    /**
-    * convention de nommage :
-    * Les membres private sont préfixés avec un _
-    */
     private _password: String = "";
+    readonly signup_date: Date = new Date(Date.now());
 
     constructor(username: String, email: String) {
         this.username = username;
@@ -44,12 +51,51 @@ class Member {
             `${author.first_name}.${author.last_name}@acme.com`,
         );
     }
-}
+};
 
 
 let domenico = Member.fromAuthor(author);
-
-console.assert(domenico.email === "Domenico.Losurdo@acme.com");
 domenico.password = "test";
 
+console.assert(domenico.email === "Domenico.Losurdo@acme.com");
 console.assert(domenico.password === "test");
+
+
+
+const user1: IPerson = {
+    username: Member.fromAuthor(author)["username"],
+    email: Member.fromAuthor(author)["email"],
+    signup_date: Member.fromAuthor(author)["signup_date"],
+}
+
+console.assert(user1.email === "Domenico.Losurdo@acme.com");
+console.assert(user1.username === "Domenico.Losurdo");
+
+
+
+// Héritage
+enum Shapes {
+    Undefined = 0,
+    Polygones = 1,
+    Circle = 2,
+    Straight = 3,
+    Segment = 4,
+};
+
+class Shape {
+    type: Shapes = Shapes.Undefined;
+};
+
+class Rectangle extends Shape {
+    h: Number = 0;
+    w: Number = 0;
+};
+
+class Square extends Rectangle {
+    side: Number = 0;
+};
+
+//Interface et HOF
+//interface signature sans nom
+
+//Interface et Arrays
