f7fa8b7801ede9e9db88743fdc90f7604bcc1af7
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-25 19:43:06 +0200

    add fetch async request to pokedex api

---
+++ pokemons/src/pokemons.ts
@@ -0,0 +1,50 @@
+// https://rapidapi.com/guides/node-fetch-typescript
+// https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+
+const fetchPokemonList = async (): Promise<any> => {
+    const apiUrl = 'https://pokeapi.co/api/v2/ability/?limit=20&offset=20';
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const extract_id_from_url = (url: String): Number => parseInt(
+    url.replace("https://pokeapi.co/api/v2/ability/", "")
+        .replace("/", "")
+)
+
+/**
+ * min id pokemon 1
+ * max id pokemon 299
+ */
+
+export const fetchAndLogPokemonList = async () => {
+    try {
+        console.log("Liste des Pokémon du Pokédex: $name, $url");
+        (await fetchPokemonList())
+            .map((it: { name: String; url: String; }) => [
+                extract_id_from_url(it.url), it.name
+            ])
+            .forEach((element: Array<String | Number>) => {
+                console.log(element[0] + ", " + element[1]);
+            });
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+};
+
+// Utilisation de la fonction asynchrone avec await en appelant la constante
+//   fetchAndLogPokemonList();
+
+// https://pokeapi.co/api/v2/ability/21 => {id,name,{names:{"language": { name, url}}}
+
