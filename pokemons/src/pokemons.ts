5d77f069d4e1bd56c2d861348ea563dbe156943c
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-25 20:13:19 +0200

    functional refactor

--- pokemons/src/pokemons.ts
+++ pokemons/src/pokemons.ts
@@ -1,38 +1,32 @@
-const fetchPokemonList = async (): Promise<any> => {
-    const apiUrl = 'https://pokeapi.co/api/v2/ability/?limit=20&offset=20';
+type Result<T, E> = { type: 'success'; value: T } | { type: 'error'; error: E };
 
-    try {
-        const response = await fetch(apiUrl);
+const fetchPokemonList = async (): Promise<Result<any[], string>> => {
+  const apiUrl = 'https://pokeapi.co/api/v2/ability/?limit=20&offset=20';
 
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data.results;
-    } catch (error) {
-        throw error;
-    }
+  return fetch(apiUrl)
+    .then(response => response.ok
+      ? response.json()
+      : Promise.reject(`Network response was not ok: ${response.status}`)
+    )
+    .then(data => ({ type: 'success', value: data.results }))
+    .catch(error => ({ type: 'error', error: error.message }));
 };
 
-/**
- * min id pokemon 1
- * max id pokemon 299
- */
-
 export const fetchAndLogPokemonList = async () => {
-    try {
-        (await fetchPokemonList())
-            .map((it: { name: String; url: String; }) => [
-                parseInt(it.url.replace("https://pokeapi.co/api/v2/ability/", "")
-                    .replace("/", "")), it.name
-            ])
-            .forEach((element: Array<String | Number>) => {
-                console.log(element[0] + ", " + element[1]);
-            });
-    } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
-    }
+  await fetchPokemonList()
+    .then(result => result.type === 'success'
+      ? result.value.map((it: { name: string; url: string }) => [
+          parseInt(it.url.replace("https://pokeapi.co/api/v2/ability/", "").replace("/", "")),
+          it.name,
+        ])
+      : []
+    )
+    .then(mappedResults => mappedResults.forEach((element: Array<string | number>) => {
+      console.log(element[0] + ", " + element[1]);
+    }))
+    .catch(error => {
+      console.error('Une erreur s\'est produite :', error);
+    });
 };
 
 fetchAndLogPokemonList();
