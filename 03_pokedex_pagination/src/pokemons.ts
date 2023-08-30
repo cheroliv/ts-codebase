7692f6130f78335285ddd3534f82a3ad83f32ddc
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-30 09:07:51 +0200

    ok

--- 03_pokedex_pagination/src/pokemons.ts
+++ 03_pokedex_pagination/src/pokemons.ts
@@ -84,5 +84,47 @@ document.querySelector<HTMLDivElement>('#stack')!.innerHTML = `
 
 const pokemons: Array<Pokemon> = await fetch_pokemons();
 console.assert(pokemons.length == 358);
-console.table(pokemons);
 display_pokemons(pokemons);
+
+interface Cursor {
+  position: Number;
+  pokemons: Pokemon[];
+};
+
+const first_cursor: Cursor = {
+  position: 1,
+  pokemons: pokemons.slice(0, 10),
+};
+
+const last_cursor: Cursor = {
+  position: Math.floor(pokemons.length / 10) + 1,
+  pokemons: pokemons.slice(
+    Math.floor(pokemons.length / 10) * 10 + 1,
+    pokemons.length),
+};
+
+const next_pokemon = (cursor: Cursor): Cursor => {
+  return {
+    position: cursor.position as number + 1,
+    pokemons: pokemons.slice(
+      (cursor.position as number) * 10 + 1,
+      (cursor.position as number) * 10 + 1 + 10)
+  };
+}
+
+const prev_pokemon = (cursor: Cursor): Cursor => {
+  return {
+    position: cursor.position as number + 1,
+    pokemons: pokemons.slice(
+      (cursor.position as number) * 10 + 1,
+      (cursor.position as number) * 10 + 1 + 10)
+  };
+};
+
+const pokemon_navigation = () => {
+  console.table(next_pokemon(first_cursor));
+  console.table(prev_pokemon(last_cursor));
+};
+
+pokemon_navigation();
+
